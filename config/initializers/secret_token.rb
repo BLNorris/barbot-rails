7b5d240ace21a1749352027f547d6e0c0e8ba329
BLNorris <BLNorris@gmail.com>
2014-04-07 10:33:17 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BarbotRails::Application.config.secret_token = '98ffaa1eca98469eefa67a6fca9c0bdaafd4023af803e49e07e4fb4cdcc0ead128134563659a4c3e900624b3c47cbf9c3c46e57c2320e634a60132d98ea9f962'
