81d113766ff22c9db4c9a80724476e8a80af8e98
BLNorris <BLNorris@gmail.com>
2014-04-11 10:23:36 -0500

    animated about page

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,13 +3,16 @@ class PublicController < ApplicationController
   def index
 
     if user_signed_in?
-      redirect_to "/recipes/all"
+      redirect_to "/recipes/all", format: 'js'
     end
     
   end
   
   def about
-    
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
   
 end
